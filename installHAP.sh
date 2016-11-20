02bbff6a81be5a4ba7f032dbb0d89b0858342ca0
mrcsmxms <marcusassenheimer@MacBookPro-Marcus.fritz.box>
2016-11-20 16:21:08 +0100

    installation skript to fully install HAP by KhaosT

---
+++ installHAP.sh
@@ -0,0 +1,39 @@
+echo "
+  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    ░░░░▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░░░
+  ░░░░░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░░░░░▒▒░░░░░░░░
+    ░░░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░▒▒░░░░░░▒▒▒▒▒▒░░░░░░
+  ░░░░░░▒▒░░▒▒░░▒▒░░▒▒▒▒░░░░▒▒░░░░░░░░░░▒▒░░░░
+    ░░░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░░░
+  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    ░░▒▒▒▒▒▒▒▒▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░
+  ░░░░▒▒░░▒▒░░▒▒░░░▒▒▒▒░░░▒▒░░▒▒░░▒▒░░▒▒░░░░░░
+    ░░▒▒░░▒▒░░▒▒░░░░▒▒░░░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░░░
+  ░░░░▒▒░░▒▒░░▒▒░░░▒▒▒▒░░░▒▒░░▒▒░░▒▒░░░░░░▒▒░░
+    ░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░░░
+  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                        // github.com/mrcsmxms
+"
+sudo apt-get update
+sudo apt-get install git-core libnss-mdns libavahi-compat-libdnssd-dev -y
+sudo apt-get remove nodejs-legacy -y
+sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+node -v
+npm -v
+sudo npm install -g node-gyp
+sudo npm install -g forever
+sudo git clone https://github.com/KhaosT/HAP-NodeJS.git
+cd HAP-NodeJS
+sudo npm install node-persist
+sudo npm rebuild
+sudo npm install debug
+sudo npm install mdns
+sudo npm install fast-srp-hap
+sudo npm install ed25519
+sudo npm install buffer-shims
+sudo npm install curve25519-n
+sudo npm install ip
+sudo npm install wiring-pi
+sudo npm install python-shell
+sudo node Core.js
